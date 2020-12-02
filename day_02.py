56c714cfbae68df17c12d450bb3a37b3e60643cd
Alexander Görtz <goertz@fuzzy.de>
2020-12-02 09:00:02 +0100

    Update day 2 solution by using regex and generator expressions

--- day_02.py
+++ day_02.py
@@ -1,25 +1,24 @@
+#%% Parsing helper
+import re
+from typing import Tuple
+
+parse_expression = re.compile(r"^(\d+)-(\d+)\s+(\w):\s+(.*)$")
+
+
+def parse_entry(entry: str) -> Tuple:
+    groups = parse_expression.match(entry).groups()
+    return *map(int, groups[:2]), *groups[2:]
+
+
 #%% check how may passwords are valid (Part 1)
-counter = 0
 with open("day_02_input.txt") as input_data:
-    for x in input_data:
-        definition, password = x.split(": ")
-        bounds, character = definition.split()
-        min_bound, max_bound = map(int, bounds.split("-"))
-        occurrence = len([x for x in password if x == character])
-        if min_bound <= occurrence <= max_bound:
-            counter += 1
-print(f"We found {counter} valid passwords in the database.")
+    result = sum(f <= p.count(c) <= s for f, s, c, p in map(parse_entry, input_data))
+print(f"We found {result} valid passwords in the database.")
 
 #%% check how may passwords are valid (Part 2)
-counter = 0
 with open("day_02_input.txt") as input_data:
-    for x in input_data:
-        definition, password = x.split(": ")
-        positions, character = definition.split()
-        first_pos, second_pos = map(int, positions.split("-"))
-        pw_length = len(password)
-        first_match = password[first_pos - 1] == character if pw_length >= first_pos else False
-        second_match = password[second_pos - 1] == character if pw_length >= second_pos else False
-        if first_match != second_match:
-            counter += 1
-print(f"We found {counter} valid passwords in the database.")
+    result = sum(
+        (p[f - 1] == c) != (p[s - 1] == c)
+        for f, s, c, p in map(parse_entry, input_data)
+    )
+print(f"We found {result} valid passwords in the database.")
