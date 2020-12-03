c00d98749337f27dfcf523d273aac14deb2f6acc
Alexander Görtz <goertz@fuzzy.de>
2020-12-03 10:15:26 +0100

    Fix memory problem for huge maps without sacrificing performance

--- day_03.py
+++ day_03.py
@@ -5,14 +5,20 @@ import math
 slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
 with open("day_03_input.txt") as input_data:
-    tree_map = np.array([[0 if c == "." else 1 for c in row.strip()] for row in input_data])
+    tree_map = np.array(
+        [[c == "#" for c in row.strip()] for row in input_data], dtype=bool
+    )
 height, width = tree_map.shape
 
-tree_map = np.tile(tree_map, math.ceil(height / width * max(s[0] for s in slopes)))
-
 #%% Count the trees (Part 1)
-print(f"Number of trees encountered: {sum(tree_map[:, 0::3].diagonal())}")
+result = sum(tree_map[np.arange(height), np.arange(0, height * 3, 3) % width])
+print(f"Number of trees encountered: {result}")
 
 #%% Count the trees for different slopes (Part 2)
-result = math.prod(sum(tree_map[0::d, 0::r].diagonal()) for r, d in slopes)
+slope_index_generator = (
+    (np.arange(0, height, d), np.arange(0, math.ceil(height / d) * r, r) % width)
+    for r, d in slopes
+)
+
+result = math.prod(sum(tree_map[s]) for s in slope_index_generator)
 print(f"The product of tree encounters on different slopes is: {result}")
