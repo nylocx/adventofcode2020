3273ac980476b26089cadbc7bf7481166f543df1
Alexander Görtz <goertz@fuzzy.de>
2020-12-08 07:34:53 +0100

    Add alternative solution and benchmark for day 7

--- day_07.py
+++ day_07.py
@@ -1,24 +1,36 @@
 #%% Day 07 Setup
-from collections import defaultdict, ChainMap
+from collections import defaultdict
+from functools import lru_cache
 import re
 
-
-# plaid brown bags contain 4 dull teal bags, 2 wavy beige bags.
-# wavy yellow bags contain no other bags.
 bag_regex = r"^(\w+ \w+)"
 contained_regex = r"(\d+) (\w+ \w+)"
 
 
-def parse_rule(input_string: str) -> dict:
+def parse_rule(input_string: str) -> tuple:
     bag_match = re.match(bag_regex, input_string).group(1)
     contained_match = [m.groups() for m in re.finditer(contained_regex, input_string)]
-    return {bag_match: [(int(m[0]), m[1]) for m in contained_match]}
+    return bag_match, [(int(m[0]), m[1]) for m in contained_match]
 
 
 with open("day_07_input.txt") as input_data:
-    bag_map = ChainMap(*map(parse_rule, input_data))
+    bag_map = dict(map(parse_rule, input_data))
 
-#%% Part 1
+
+#%% Part 1 recursive slow
+@lru_cache()
+def contains_bag(query_bag: str, bags: set[str]) -> bool:
+    if not bags:
+        return False
+    if query_bag in bags:
+        return True
+    return any(contains_bag(query_bag, {b for _, b in bag_map[bag]}) for bag in bags)
+
+
+result = sum(contains_bag("shiny gold", s) for s in ({b for _, b in bags} for _, bags in bag_map.items()))
+print(f"Bags that can contain a shiny gold bag: {result}")
+
+#%% Part 1 a bit faster
 lookup_dict = defaultdict(set)
 for bag, contained_bags in bag_map.items():
     for _, contained_bag in contained_bags:
@@ -35,7 +47,7 @@ print(f"Bags that can contain a shiny gold bag: {len(bags_containing_shiny_gold)
 
 
 #%% Part 2
-def get_sub_bag_count(count: int, bag: str):
+def get_sub_bag_count(count: int, bag: str) -> int:
     if not bag_map[bag]:
         return count
     return count + count * sum(get_sub_bag_count(c, b) for c, b in bag_map[bag])
