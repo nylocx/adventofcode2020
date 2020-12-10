13baf148844b55cd97c059b018bab9e03095ba8e
Alexander Görtz <goertz@fuzzy.de>
2020-12-10 07:41:13 +0100

    Enhance day 9 performance

--- day_09.py
+++ day_09.py
@@ -8,24 +8,18 @@ with open("day_09_input.txt") as input_data:
 
 x = np.lib.stride_tricks.as_strided(numbers, (len(numbers) - offset, offset), (numbers.strides[0], numbers.strides[0]))
 
-for c, p in zip(numbers[25:], x):
-    if not (np.triu(p.reshape(-1, 1) + p.reshape(1, -1), 1) == c).any():
+for c, p in zip(numbers[25:], np.expand_dims(x, axis=1)):
+    if not (p + p.T == c).any():
+        print(p.shape)
         result_p1 = c
         break
 
 print(f"The first number not composed from two of the {offset} previous is {result_p1}")
 
 #%% Part 2
-accumulated = numbers.cumsum()
-found = False
-for left in range(len(numbers) - 2):
-    if found:
-        break
-    for right in range(left + 2, len(numbers)):
-        if accumulated[right] - accumulated[left] == result_p1:
-            involved = numbers[left + 1:right + 1]
-            result_p2 = min(involved) + max(involved)
-            found = True
-            break
-
-print(f"The sum of first and last element from the interval summing up tp {result_p1} is {result_p2}")
+accumulated = numbers.cumsum().reshape(1, -1)
+for left, right in zip(*np.nonzero((accumulated - accumulated.T) == result_p1)):
+    if right - left > 2:
+        involved = numbers[left + 1:right + 1]
+        result_p2 = min(involved) + max(involved)
+        print(f"The sum of first and last element from the interval summing up tp {result_p1} is {result_p2}")
